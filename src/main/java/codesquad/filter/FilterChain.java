d3375dc13afbe7d62b7d419ae2859efb737eac76
yudonggeun <ydong98@gmail.com>
2024-07-03 11:22:12 +0900

    docs : FilterChain javadoc 추가

--- src/main/java/codesquad/filter/FilterChain.java
+++ src/main/java/codesquad/filter/FilterChain.java
@@ -3,7 +3,16 @@ package codesquad.filter;
 import codesquad.http.HttpRequest;
 import codesquad.http.HttpResponse;
 
+/**
+ * 필터 체인은 필터의 실행 순서를 제어하는 책임을 가지고 있습니다.
+ */
 public interface FilterChain {
 
+    /**
+     * 필터 체인에서 관리되는 필터를 정해진 순서에 따라서 실행하는 메서드입니다.
+     *
+     * @param request  HTTP 요청
+     * @param response HTTP 응답
+     */
     void doFilter(HttpRequest request, HttpResponse response);
 }
--- src/main/java/codesquad/filter/FilterConfig.java
+++ src/main/java/codesquad/filter/FilterConfig.java
@@ -3,6 +3,21 @@ package codesquad.filter;
 import java.util.Set;
 import java.util.TreeSet;
 
+/**
+ * <h2>
+ * FilterChain 생성을 위한 설정 정보를 담고 있는 클래스입니다.
+ * </h2>
+ *
+ * <p>
+ * Filter는 어플리케이션의 가동 시점에서 결정되어야하는 설정 정보이고 전역으로 적용되는 되어야 하는 요구사항을 가지고 있습니다.
+ * 이 요구사항을 만족하기 위해서 FilterConfig는 생성할 FilterChain에 대한 필요한 정보를 가지고 있습니다.
+ * Filter의 우선순위에 따라서 실행 순서가 결정 되기 때문에 추가된 필터의 우선순위를 고려하여 필터를 입력해주세요.
+ * </p>
+ *
+ * @author yudonggeun
+ * @see FilterChain
+ * @see Filter
+ */
 public class FilterConfig {
 
     private static final Set<Filter> filters = new TreeSet<>();
