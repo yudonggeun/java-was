51ed6d374cdd8b231a16c1cfba8dd3be656ba361
yudonggeun <ydong98@gmail.com>
2024-07-04 16:46:22 +0900

    fix : response body가 없는 응답시 에러 반환 오류 수정

--- src/main/java/codesquad/filter/CharSetFilter.java
+++ src/main/java/codesquad/filter/CharSetFilter.java
@@ -2,12 +2,15 @@ package codesquad.filter;
 
 import codesquad.http.HttpRequest;
 import codesquad.http.HttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 응답의 Content-Type이 text인 경우 charset=utf-8을 추가하는 필터입니다.
  */
 public class CharSetFilter implements Filter {
 
+    private final Logger logger = LoggerFactory.getLogger(CharSetFilter.class);
     private final String charSet;
 
     public CharSetFilter(String charSet) {
@@ -18,7 +21,8 @@ public class CharSetFilter implements Filter {
     public void postHandle(HttpRequest request, HttpResponse response) {
         String contentType = response.getHeader("Content-Type");
         if (contentType == null) {
-            throw new IllegalArgumentException("Content-Type이 없습니다.");
+            logger.debug("Content-Type is null");
+            return;
         }
         if (contentType.startsWith("text")) {
             response.addHeader("Content-Type", contentType + "; charset=" + charSet);
