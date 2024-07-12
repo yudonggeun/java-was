5caf15544a1d1dca5479f336b8c4fb3e2a663953
yudonggeun <ydong98@gmail.com>
2024-07-12 09:56:53 +0900

    feat : 임시 관리자 계정 추가

--- src/main/java/codesquad/application/repository/MyRepository.java
+++ src/main/java/codesquad/application/repository/MyRepository.java
@@ -2,12 +2,17 @@ package codesquad.application.repository;
 
 import codesquad.application.domain.User;
 
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class MyRepository {
 
     public static final MyRepository source = new MyRepository();
+
+    public MyRepository() {
+        this.map.put("admin", new User("admin", "admin", "admin"));
+    }
 
     private final Map<String, Object> map = new ConcurrentHashMap<>();
 
@@ -20,7 +25,14 @@ public class MyRepository {
         return (User) map.get(key);
     }
 
+    public List<User> findAllUser() {
+        return map.values().stream()
+                .filter(value -> value instanceof User)
+                .map(value -> (User) value).toList();
+    }
+
     public void delete(String key) {
         map.remove(key);
     }
+
 }
